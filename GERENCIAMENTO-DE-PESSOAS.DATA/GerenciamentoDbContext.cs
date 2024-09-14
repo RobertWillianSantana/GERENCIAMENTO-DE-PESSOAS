daa18ce9509ae5b94ec716e7885510ba968a68a7
RobertWillianSantana <robertwillian4646@gmail.com>
2024-09-14 11:27:03 -0300

    feat: Classe de contexto

---
+++ GERENCIAMENTO-DE-PESSOAS.DATA/GerenciamentoDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GERENCIAMENTO_DE_PESSOAS.DATA
+{
+    public class GerenciamentoDbContext
+    {
+        
+    }
+}
